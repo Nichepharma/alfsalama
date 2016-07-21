94d1b51e0310dad1903aaa33af5984a88781fa8c
Ahmad <ahmedelmalah@hotmail.com>
2016-07-21 16:20:27 +0200

    datalink

--- app/Http/Controllers/DatalinkController.php
+++ app/Http/Controllers/DatalinkController.php
@@ -6,12 +6,69 @@ use App\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 
+use App;
+
 class DatalinkController extends Controller {
   public function index()
   {
       $action = Input::get('action');
-      $code = Input::get('code');
       $data = json_decode(Input::get('data'));
-      return $data->name;
+
+      switch ($action) {
+        case "get_countries":
+          $countries = App\Country::all();
+          return $countries;
+
+        case "get_cities":
+          $cities = App\Country::find($data->country_id)->cities;
+          return $cities;
+
+        case "get_specs":
+          $specs = App\Spec::all();
+          return $specs;
+
+        case "check_email":
+          $user = App\User::where('email', $data->email)->get();
+          return count($user);
+
+        case "put_patient":
+          $user = App\User::create([
+            'name'       => $data->name,
+            'email'      => $data->email,
+            'password'   => $data->password,
+            'role_id'    => 2,
+            'country_id' => $data->country_id,
+            'city_id'    => $data->city_id,
+            'address'    => $data->address,
+            'contact'    => $data->contact
+          ]);
+
+          $patient = new App\Patient;
+            $patient->user_id = $user->id;
+          $patient->save();
+          return 'Done';
+
+        case "put_doctor":
+          $user = App\User::create([
+            'name'       => $data->name,
+            'email'      => $data->email,
+            'password'   => $data->password,
+            'role_id'    => 2,
+            'country_id' => $data->country_id,
+            'city_id'    => $data->city_id,
+            'address'    => $data->address,
+            'contact'    => $data->contact
+          ]);
+          $doctor = new App\Doctor;
+            $doctor->user_id = $user->id;
+            $doctor->spec_id = $data->spec_id;
+          $doctor->save();
+          return 'Done';
+
+        case 'login':
+          //$user = App\User::where('')
+          return;
+      }
+
   }
 }
--- resources/views/protocols.blade.php
+++ resources/views/protocols.blade.php
@@ -3,19 +3,66 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">ALFSALAMA Protocols</div>
+              <p align="center">
+                <b>ALFSALAMA Protocols Documentation</b>
+              </p>
+              <p>Datalink URL : <a href="http://www.alfsalama.com/datalink">http://www.alfsalama.com/datalink</a></p>
+              <p>Request Example : http://www.alfsalama.com/datalink?action=get_cities&data={"country_id":"1"}</p>
 
                 <div class="panel-body">
-                    <ul>
-                      <li>Get Countries</li>
-                      test
-                      <li>Get The Cities Of A Country</li>
-                      test
-                      <li>Register a new patient</li>
-                      test
-                      <li>Register a new doctor</li>
-                      test
-                    <ul>
+                    <table border="1" style="table" width="100%">
+                      <thead>
+                        <th>Section</th>
+                        <th>Function Description</th>
+                        <th>Action</th>
+                        <th>Data Example</th>
+                        <th>Samples Response</th>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td rowspan="7" align="center">Registration & Login</td>
+                          <td>Get Countries</td>
+                          <td>get_countries</td>
+                          <td></td>
+                          <td>[{"id":1,"name":"Egypt"}]</td>
+                        </tr>
+                        <tr>
+                          <td>Get The Cities Of A Country</td>
+                          <td>get_cities</td>
+                          <td>{"country_id":"1"}</td>
+                          <td>[{"id":1,"name":"Alexandria","country_id":1},{"id":2,"name":"Cairo","country_id":1}]</td>
+                        </tr>
+                        <tr>
+                          <td>Get Doctors Specialities</td>
+                          <td>get_specs</td>
+                          <td></td>
+                          <td>[{"id":1,"name":"Orth"},{"id":2,"name":"Pud"}]</td>
+                        </tr>
+                        <tr>
+                          <td>Check Availability of an email</td>
+                          <td>check_email</td>
+                          <td>{"email":"admin@admin.com"}</td>
+                          <td>1 //means it's found</td>
+                        </tr>
+                        <tr>
+                          <td>Register A New Patient</td>
+                          <td>put_patient</td>
+                          <td>{"name":"anyname","email":"anyemail","password":"anypassword","country_id":"1","city_id":"2","address":"anyaddress","contact":"anycontact"}</td>
+                          <td>Done</td>
+                        </tr>
+                        <tr>
+                          <td>Register A New Doctor</td>
+                          <td>put_doctor</td>
+                          <td>{"name":"anyname","email":"anyemail","password":"anypassword","country_id":"1","city_id":"2","address":"anyaddress","contact":"anycontact","spec_id":"2"}</td>
+                          <td>Done</td>
+                        </tr>
+                        <tr>
+                          <td>Login</td>
+                          <td>login</td>
+                          <td>{"email":"aaa@aaa.com", "password":"123456"}</td>
+                        </tr>
+                      </tbody>
+                    </table>
                 </div>
             </div>
         </div>
